46d8b4030c7bbfe1433fbda168874dd5d0d81417
Chris Stone <contact@chrisstone.dev>
2023-03-26 14:39:13 -0400

    Changed and new talks

--- tests/devnexus.spec.ts
+++ tests/devnexus.spec.ts
@@ -33,7 +33,7 @@ test('no changes have been made to the Devnexus 2023 schedule', async ({page}) =
             {room: '304', name: 'Cross-Platform Cloud CI/CD'},
             {room: '305', name: 'Cooking Recipes, Coding, an IDE and YOU'},
             {room: '311', name: 'How to report a vulnerability: Responsible Disclosure for Developers'},
-            {room: '312', name: 'Quarkus for Spring Developers'},
+            {room: '312', name: 'Live Coding Quarkus: Supersonic, Subatomic Kubernetes-Native Java'},
             {
               room: '313',
               name: 'Let\'s get Functional: The fast path to smart serverless with Spring Cloud Function & Azure Functions',
@@ -42,10 +42,7 @@ test('no changes have been made to the Devnexus 2023 schedule', async ({page}) =
             {room: '315', name: 'The Cloud Native Compiler: JIT-as-a-Service'},
             {room: '402', name: 'Functional Kotlin'},
             {room: '403', name: 'Just Code isn\'t enough, A successful project needs more....'},
-            {
-              room: 'Sydney Marcus',
-              name: 'Walking to School in the Snow or What to Do When You Aren\'t Building Bridges with Bricks',
-            },
+            {room: 'Sydney Marcus', name: 'Scaling Batch Applications '},
           ],
         },
         {time: '12:30', talks: [{room: 'Foyer', name: 'Lunch'}]},
@@ -56,10 +53,10 @@ test('no changes have been made to the Devnexus 2023 schedule', async ({page}) =
             {room: '304', name: 'Vue for React Developers'},
             {room: '305', name: 'Know your Java?'},
             {room: '311', name: 'Wargames - Java vulnerabilities and why you should care'},
-            {room: '312', name: 'Quarkus Unleashed'},
+            {room: '312', name: 'Quarkus for Spring Developers'},
             {room: '313', name: 'Natively Cloud Native - Building Agile Microservices with Micronaut'},
             {room: '314', name: 'Beyond REST and CRUD: Integration patterns in Microservices'},
-            {room: '315', name: 'Patterns, Predictions, and Prescriptions'},
+            {room: '315', name: 'Patterns, Predictions, and Programming '},
             {room: '402', name: 'Writing Gradle plugins that users will love'},
             {room: '403', name: 'Developer Productivity - clean | secure | fast'},
             {room: 'Sydney Marcus', name: 'Beginners guide to balance your data across Apache Kafka partitions'},
@@ -73,7 +70,10 @@ test('no changes have been made to the Devnexus 2023 schedule', async ({page}) =
             {room: '304', name: 'From Your Perspective'},
             {room: '305', name: 'How to Fix Your Technical Debt Dilemma'},
             {room: '311', name: 'Empowering Developers to Embrace Security'},
-            {room: '312', name: 'From Code To Production - How Developer Tools Make you Successful'},
+            {
+              room: '312',
+              name: 'Hyper-Automate your Cloud Native Journey with Kubernetes Custom Resources and Operators',
+            },
             {room: '313', name: 'Progressive Delivery with Kubernetes and Argo Rollouts'},
             {room: '314', name: 'Spring Modulith: What the what?'},
             {room: '315', name: 'Enhancing Java applications for the cloud'},
@@ -95,13 +95,16 @@ test('no changes have been made to the Devnexus 2023 schedule', async ({page}) =
             {room: '304', name: 'Properly Managing Data In React'},
             {room: '305', name: 'Refactor your code to Java 20'},
             {room: '311', name: 'Securing Your Software Supply Chain One Open Source Project at a Time'},
-            {room: '312', name: 'Batch Processing Cloud Apps with Java'},
+            {
+              room: '312',
+              name: 'KEDA, HPA, and VPA? An Introduction to Scaling a Event-Driven Workloads on Kubernetes',
+            },
             {
               room: '313',
               name: 'Weathering the Cloud Storm: Building Resilient Geo-Distributed Apps with Spring Cloud',
             },
             {room: '314', name: 'Bootiful Spring Boot 3'},
-            {room: '315', name: 'Java Platform TBD'},
+            {room: '315', name: 'Asynchronous Programming in Java using Virtual Threads'},
             {room: '402', name: 'Mastering the Linux command line'},
             {room: '403', name: 'Observing Minecraft'},
             {room: 'Sydney Marcus', name: 'Machine Learning Data Pipelines with Kafka and Tensorflow'},
@@ -125,10 +128,11 @@ test('no changes have been made to the Devnexus 2023 schedule', async ({page}) =
         },
         {
           time: '10:00', talks: [
-            {room: '302', name: 'CI/CD Pipelines : What, Why, How?'},
+            {room: '302', name: 'Spring Cloud Gateway: What is it and What\'s New.'},
             {room: '303', name: 'Jakarta EE 10 and Beyond'},
             {room: '304', name: 'Deceptive Patterns & FAST'},
             {room: '305', name: 'Loom Virtual Threads in the JDK 20'},
+            {room: '311', name: 'Don\'t Trust anyone.... Secure your Microservices with ZeroTrust approach.'},
             {room: '312', name: 'Java to Kubernetes for dummies'},
             {room: '313', name: 'Introduction to Rancher Desktop'},
             {room: '314', name: 'Apache Pulsar Development 101 with Java'},
@@ -147,7 +151,7 @@ test('no changes have been made to the Devnexus 2023 schedule', async ({page}) =
             {room: '305', name: 'To Java 20 and Beyond!'},
             {room: '311', name: 'A Community Approach to OSS Distribution'},
             {room: '312', name: 'Change Data Streaming Patterns in Distributed Systems'},
-            {room: '313', name: 'Don\'t Trust anyone.... Secure your Microservices with ZeroTrust approach.'},
+            {room: '313', name: 'Knative Core Concepts! And Spiders!'},
             {room: '314', name: 'Introducing Spring for Apache Pulsar'},
             {room: '315', name: 'Leading Edge! Changing the Java World with Loom, Panama, and More'},
             {room: '402', name: 'The Programmer\'s Guide to JDK Flight Recorder'},
@@ -158,7 +162,7 @@ test('no changes have been made to the Devnexus 2023 schedule', async ({page}) =
         {time: '12:30', talks: [{room: 'Foyer', name: 'Lunch'}]},
         {
           time: '13:30', talks: [
-            {room: '302', name: 'Spring Cloud Gateway: What is it and What\'s New.'},
+            {room: '302', name: 'Next up: Spring Security 6'},
             {room: '303', name: 'Jakarta EE integration testing'},
             {
               room: '304',
@@ -194,7 +198,7 @@ test('no changes have been made to the Devnexus 2023 schedule', async ({page}) =
         },
         {
           time: '16:00', talks: [
-            {room: '302', name: 'Next up: Spring Security 6'},
+            {room: '302', name: 'CI/CD Pipelines : What, Why, How?'},
             {room: '303', name: 'Let\'s take a look at how a Jakarta EE cloud-native application should look!'},
             {room: '304', name: 'Creating a Modern Web App Using Spring Boot and Vue.js with JHipster'},
             {room: '305', name: 'Revisiting Design Patterns after 20'},
